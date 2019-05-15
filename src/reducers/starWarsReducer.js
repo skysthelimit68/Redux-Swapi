e5ed7331148b2c750f766690300529f172221326
Ming Liu <mliu@macpro.home>
2019-05-15 14:32:05 -0400

    action, reducer, ...

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,38 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING_DATA, FETCHING_SUCCESS, FETCHING_FAIL } from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error:""
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
+  console.log(action.type);
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING_DATA: 
+      return {
+        ...state,
+        fetching : true,
+        error : ""
+      }
+      case FETCHING_SUCCESS:
+        return {
+          ...state,
+          fetching : false,
+          characters : action.payload
+        }
+      case FETCHING_FAIL:
+        return {
+          ...state,
+          fetching : false,
+          error: action.payload
+        }
     default:
       return state;
   }
+    
+    // Fill me in with the important reducers
+    // action types should be FETCHING, SUCCESS and FAILURE
+    // your switch statement should handle all of these cases.
+    
+  
 };
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import fetchingData from "../actions";
 // import actions
 
 class CharacterListView extends React.Component {
@@ -10,11 +11,13 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
+    this.props.fetchingData();
     // call our action
   }
 
   render() {
     if (this.props.fetching) {
+      console.log("currently fetching data")
       // return something here to indicate that you are fetching data
     }
     return (
@@ -25,11 +28,17 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => ({
+  characters : state.characters,
+  fetching : state.fetching,
+})
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps, /* mapStateToProps replaces null here */
   {
+    fetchingData
     /* action creators go here */
   }
 )(CharacterListView);
